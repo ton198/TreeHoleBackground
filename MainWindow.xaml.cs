524d92bc6ae7d22a6d1764f66a495fe0fc7f8adc
Tony Li <24tony.li@beijing101id.com>
2023-10-31 10:24:01 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using MySql.Data.MySqlClient;
+using LoadingWidget;
+
+namespace TreeHoleBackStage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<DataTemplate> Datas { get; set; }
+        private readonly MySqlConnection conn;
+        readonly List<WritingWindow> writingWindows;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Datas = new ObservableCollection<DataTemplate>();
+            const string connectKey = "Database=tree_hole;Data Source=101.43.188.94;port=3306;User Id=root;Password=Ton@8177919;Charset=utf8";
+            conn = new MySqlConnection(connectKey);
+            writingWindows = new List<WritingWindow>();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            conn.Open();            
+
+            dataList.ItemsSource = Datas;
+            FreshData();
+        }
+
+        private void FreshData()
+        {
+            MySqlCommand cmd = new MySqlCommand("select * from main where answer like 'Have not answer yet,%'", conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            for (; Datas.Count > 0; Datas.RemoveAt(Datas.Count - 1)); // remove all of the elements
+            
+            while (reader.Read())
+            {
+                DataTemplate dataTemplate = new DataTemplate(writingWindows)
+                {
+                    ID = reader.GetString("user_id"),
+                    Question = reader.GetString("question"),
+                    Reply = reader.GetString("answer")
+                };
+                dataTemplate.OnReplied += DataTemplate_OnReplied;
+                Console.WriteLine(dataTemplate);
+                Datas.Add(dataTemplate);
+            }
+            reader.Close();
+        }
+
+        private void DataTemplate_OnReplied(string? id, string? reply)
+        {
+            string sql = "update main set answer='" + reply + "' where user_id='" + id + "'";
+            MySqlCommand command = new MySqlCommand(sql, conn);
+            command.ExecuteNonQuery();
+            command.Dispose();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure to exit this application?", "Warning", MessageBoxButton.YesNo);
+            if (result != MessageBoxResult.Yes)
+            {
+                e.Cancel = true;
+                return;
+            }
+            conn.Close();
+            System.Environment.Exit(0);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (writingWindows.Count > 0)
+            {
+                MessageBox.Show("please close all of the editing windows before refreshing");
+                return;
+            }
+            FreshData();
+        }
+
+        public class DataTemplate
+        {
+            public string? ID { get; set; }
+            public string? Question { get; set; }
+            public string? Reply { get; set; }
+
+            private readonly List<WritingWindow> writingWindows;
+            private WritingWindow? myWindow;
+
+            public DataTemplate(List<WritingWindow> writingWindows)
+            {
+                this.writingWindows = writingWindows;
+            }
+
+            public void Start()
+            {
+                myWindow = new WritingWindow(ID, Question, Reply);
+                writingWindows.Add(myWindow);
+                myWindow.OnSubmited += Window_OnSubmited;
+                myWindow.Show();
+            }
+
+            private void Window_OnSubmited(string? replyText)
+            {
+                Reply = replyText;
+                TriggerReplyed();
+                writingWindows.Remove(myWindow);
+            }
+
+            public delegate void ReplyHandler(string? id, string? replyText);
+            public event ReplyHandler? OnReplied;
+            protected void TriggerReplyed()
+            {
+                OnReplied?.Invoke(ID, Reply);
+            }
+        }
+
+        private void dataList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (dataList.SelectedIndex >= 0)
+                Datas[dataList.SelectedIndex].Start();
+        }
+
+        private void FreshAll()
+        {
+            MySqlCommand cmd = new MySqlCommand("select * from main", conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            for (; Datas.Count > 0; Datas.RemoveAt(Datas.Count - 1)) ; // remove all of the elements
+
+            while (reader.Read())
+            {
+                DataTemplate dataTemplate = new DataTemplate(writingWindows);
+                try {
+                    dataTemplate.ID = reader.GetString("user_id");
+                } catch (System.Data.SqlTypes.SqlNullValueException){}
+                try {
+                    dataTemplate.Question = reader.GetString("question");
+                } catch (System.Data.SqlTypes.SqlNullValueException){}
+                try {
+                    dataTemplate.Reply = reader.GetString("answer");
+                } catch (System.Data.SqlTypes.SqlNullValueException){}
+
+                dataTemplate.OnReplied += DataTemplate_OnReplied;
+                Console.WriteLine(dataTemplate);
+                Datas.Add(dataTemplate);
+            }
+            reader.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (writingWindows.Count > 0)
+            {
+                MessageBox.Show("please close all of the editing windows before refreshing.");
+                return;
+            }
+            FreshAll();
+        }
+    }
+}
---
+++ WritingWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TreeHoleBackStage
+{
+    /// <summary>
+    /// WritingWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class WritingWindow : Window
+    {
+        readonly string? id, question, reply;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            IDLabel.Content += id;
+            QuestionContainer.Text = question;
+            ReplyContainer.Text = reply;
+            ReplyText.SpellCheck.IsEnabled = true;
+        }
+
+        public WritingWindow(string? id, string? question, string? reply)
+        {
+            InitializeComponent();
+            this.id = id;
+            this.question = question;
+            this.reply = reply;
+        }
+
+        public delegate void SubmitHandler(string replyText);
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            TextRange textRange = new TextRange(ReplyText.Document.ContentStart, ReplyText.Document.ContentEnd);
+            TriggerSubmited(textRange.Text);
+        }
+
+        public event SubmitHandler? OnSubmited;
+        protected virtual void TriggerSubmited(string replyText)
+        {
+            OnSubmited?.Invoke(replyText);
+        }
+    }
+}
